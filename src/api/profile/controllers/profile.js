e36508f8bec012f43bec66f0d280fb6585e0c616
Your Name <mmessiasdev@gmail.com>
2023-08-18 20:18:35 -0300

    First Commit

---
+++ src/api/profile/controllers/profile.js
@@ -0,0 +1,51 @@
+// @ts-nocheck
+'use strict';
+
+/**
+ *  profile controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::profile.profile', ({Strapi}) => ({
+    async createMe(ctx) {
+        try {
+            const user = ctx.state.user;
+            if(!user){
+                return ctx.badRequest(401, [{ messages: "No athorized user found!"}]);
+            }
+            const result = await strapi.entityService.create('api::profile.profile', {
+                data: {
+                    fullname: ctx.request.body.fullname,
+                    email: user.email,
+                    user: user.id,
+                }
+            });
+            return result;
+        } catch (err){
+            return ctx.badRequest(500, [{ messages: [{ id: err.message}]}]);
+        }
+    },
+    async getMe(ctx) {
+        try {
+            const user = ctx.state.user;
+            if(!user){
+                return ctx.badRequest(401, [{ messages: "No athorized user found!"}]);
+            }
+            const result = await strapi.db.query('api::profile.profile').findOne({
+                where: {
+                    user: {
+                        id: {
+                            $eq: user.id
+                        }
+                    }
+                },
+                populate: {
+                }
+            });
+            return result;
+        } catch (err){
+            return ctx.badRequest(500, [{ messages: [{ id: 'Error'}]}]);
+        }
+    }
+}));
